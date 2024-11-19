7f505aed47eb1bcad1bdcfcd7ef8e08b1ef3a760
Alexandre Leal <alexrleal0@gmail.com>
2024-11-19 08:59:57 -0300

    Created project

---
+++ src/main/java/com/puc/trabalho1_n2_jogo_com_poo/Trabalho1N2JogoComPooApplication.java
@@ -0,0 +1,13 @@
+package com.puc.trabalho1_n2_jogo_com_poo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trabalho1N2JogoComPooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Trabalho1N2JogoComPooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/puc/trabalho1_n2_jogo_com_poo/Trabalho1N2JogoComPooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puc.trabalho1_n2_jogo_com_poo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Trabalho1N2JogoComPooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
